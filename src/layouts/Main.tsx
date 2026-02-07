dffe02eaf1fcfd32a0a417791736aa15f101c875
linzeguang <15817289730@163.com>
2026-02-07 10:35:14 +0800

    fix

--- src/layouts/Main.tsx
+++ src/layouts/Main.tsx
@@ -3,7 +3,7 @@ import { Outlet } from 'react-router'
 
 const Main: React.FC = () => {
   return (
-    <main className="flex-1">
+    <main className="viewport flex-1">
       <Outlet />
     </main>
   )
--- src/providers/I18nLocaleProvider.tsx
+++ src/providers/I18nLocaleProvider.tsx
@@ -1,16 +1,28 @@
 import { I18nProvider } from '@lingui/react'
 import { useAtomValue } from 'jotai/react'
-import React, { PropsWithChildren, useEffect } from 'react'
+import React, { PropsWithChildren, useEffect, useState } from 'react'
 
 import i18n, { importLocale } from '@/i18n'
 import { localeAtom } from '@/stores/app'
 
 const I18nLocaleProvider: React.FC<PropsWithChildren> = ({ children }) => {
   const locale = useAtomValue(localeAtom)
+  const [ready, setReady] = useState(false)
 
   useEffect(() => {
-    importLocale(locale)
+    const loadLocale = async () => {
+      await importLocale(locale)
+      setReady(true)
+    }
+
+    loadLocale()
+
+    return () => {
+      setReady(false)
+    }
   }, [locale])
+
+  if (!ready) return null
 
   return <I18nProvider i18n={i18n}>{children}</I18nProvider>
 }
