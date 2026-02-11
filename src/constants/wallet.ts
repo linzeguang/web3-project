2c7facc7442536e3ea886ffabc7a19c67ae9e328
linzeguang <15817289730@163.com>
2026-02-11 10:27:28 +0800

    bak

---
+++ src/constants/wallet.ts
@@ -0,0 +1,27 @@
+import { AppKitNetwork, bscTestnet, mainnet } from '@reown/appkit/networks'
+import { WagmiAdapter } from '@reown/appkit-adapter-wagmi'
+
+export const projectId = import.meta.env.VITE_PROJECT_ID || ''
+
+if (!projectId) {
+  throw new Error('Project ID is not defined')
+}
+
+export const metadata = {
+  name: 'Web3 Project',
+  description: 'My Web3 Application',
+  url: 'https://reown.com', // origin must match your domain & subdomain
+  icons: ['https://avatars.githubusercontent.com/u/179229932']
+}
+
+export const networks: [AppKitNetwork, ...AppKitNetwork[]] = [
+  mainnet,
+  bscTestnet
+]
+
+export const wagmiAdapter = new WagmiAdapter({
+  projectId,
+  networks
+})
+
+export const config = wagmiAdapter.wagmiConfig
--- src/providers/WalletProvider.tsx
+++ src/providers/WalletProvider.tsx
@@ -1,38 +1,19 @@
-import type { AppKitNetwork } from '@reown/appkit/networks'
 import { createAppKit } from '@reown/appkit/react'
-import { WagmiAdapter } from '@reown/appkit-adapter-wagmi'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import React, { type PropsWithChildren } from 'react'
-import { bscTestnet } from 'viem/chains'
-import { injected, WagmiProvider } from 'wagmi'
+import { WagmiProvider } from 'wagmi'
+
+import { metadata, networks, projectId, wagmiAdapter } from '@/constants/wallet'
 
 const queryClient = new QueryClient()
 
-const projectId = import.meta.env.VITE_PROJECT_ID || ''
-
-const metadata = {
-  name: 'Web3 Project',
-  description: 'My Web3 Application',
-  url: 'https://example.com', // origin must match your domain & subdomain
-  icons: ['https://avatars.githubusercontent.com/u/179229932']
-}
-
-const networks: [AppKitNetwork, ...AppKitNetwork[]] = [bscTestnet]
-
-const wagmiAdapter = new WagmiAdapter({
-  networks,
-  projectId,
-  connectors: [injected()]
-})
-
 createAppKit({
   adapters: [wagmiAdapter],
-  networks,
-  defaultNetwork: bscTestnet,
   projectId,
+  networks,
   metadata,
   features: {
-    analytics: true // Optional - defaults to your Cloud configuration
+    analytics: false // Optional - defaults to your Cloud configuration
   },
   enableCoinbase: false,
   enableWalletGuide: false
@@ -41,7 +22,10 @@ createAppKit({
 const WalletProvider: React.FC<PropsWithChildren> = ({ children }) => {
   return (
     <WagmiProvider config={wagmiAdapter.wagmiConfig}>
-      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+      <QueryClientProvider client={queryClient}>
+        <appkit-button />
+        {children}
+      </QueryClientProvider>
     </WagmiProvider>
   )
 }
